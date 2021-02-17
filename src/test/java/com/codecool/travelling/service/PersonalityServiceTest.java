e4b4d9812c2d80f5eadfe453c9ab79f098aa69aa
Sirazad <lajosimola2@gmail.com>
2021-02-18 00:30:57 +0100

    Role Ideals preset
    added a mock Role Ideals for @BeforeAll to include all trait and skill elements with the range of salesman benchmark

--- src/test/java/com/codecool/travelling/service/PersonalityServiceTest.java
+++ src/test/java/com/codecool/travelling/service/PersonalityServiceTest.java
@@ -5,6 +5,8 @@ import com.codecool.travelling.model.RoleIdeal;
 import com.codecool.travelling.repository.PersonalityRepository;
 import com.codecool.travelling.repository.PositionRepository;
 import com.codecool.travelling.repository.SalesmanRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -22,20 +24,47 @@ import static org.junit.jupiter.api.Assertions.*;
 class PersonalityServiceTest {
 
     private Personality personality = Mockito.mock(Personality.class);
-    private RoleIdeal roleIdeal = Mockito.mock(RoleIdeal.class);
+    private static RoleIdeal roleIdeal = Mockito.mock(RoleIdeal.class);
     private PositionRepository positionRepository = Mockito.mock(PositionRepository.class);
     private SalesmanRepository salesmanRepository = Mockito.mock(SalesmanRepository.class);
     private PersonalityRepository personalityRepository = Mockito.mock(PersonalityRepository.class);
 
     private PersonalityService personalityService = new PersonalityService(salesmanRepository,positionRepository,personalityRepository,roleIdeal);
 
+    @BeforeAll
+    public static void init(){
+        System.out.println("Instantiating PersonalityService Class");
+        /**
+         * Setting up personality trait elements for role ideal
+         */
+        when(roleIdeal.getStudyIndex()).thenReturn(new ArrayList<>(Arrays.asList(6,7,8,9)));
+        when(roleIdeal.getVocabulary()).thenReturn(new ArrayList<>(Arrays.asList(6,7,8)));
+        when(roleIdeal.getReadingLiteracy()).thenReturn(new ArrayList<>(Arrays.asList(7,8,9)));
+        when(roleIdeal.getCalculation()).thenReturn(new ArrayList<>(Arrays.asList(6,7,8)));
+        when(roleIdeal.getNumberComprehension()).thenReturn(new ArrayList<>(Arrays.asList(7,8,9)));
+
+        /**
+         * Setting up personality skill elements for role ideal
+         */
+        when(roleIdeal.getEnergyLevel()).thenReturn(new ArrayList<>(Arrays.asList(6,7,8)));
+        when(roleIdeal.getAssertiveness()).thenReturn(new ArrayList<>(Arrays.asList(7,8,9)));
+        when(roleIdeal.getSocialContacts()).thenReturn(new ArrayList<>(Arrays.asList(5,6,7)));
+        when(roleIdeal.getCompliance()).thenReturn(new ArrayList<>(Arrays.asList(6,7,8)));
+        when(roleIdeal.getAttitude()).thenReturn(new ArrayList<>(Arrays.asList(7,8,9)));
+        when(roleIdeal.getDecisionMaking()).thenReturn(new ArrayList<>(Arrays.asList(5,6,7)));
+        when(roleIdeal.getAdaptability()).thenReturn(new ArrayList<>(Arrays.asList(5,6,7)));
+        when(roleIdeal.getIndependence()).thenReturn(new ArrayList<>(Arrays.asList(4,5,6)));
+        when(roleIdeal.getObjectiveDecisionMaking()).thenReturn(new ArrayList<>(Arrays.asList(5,6,7)));
+    }
+
+    @AfterAll
+    public static void teardown(){
+        System.out.println("Tearing down PersonalityService test");
+    }
+
+
     @Test
     public void testCalculateDifferenceForSkill(){
-        when(roleIdeal.getStudyIndex()).thenReturn(new ArrayList<>(Arrays.asList(6,7,8,9,10)));
-        when(roleIdeal.getVocabulary()).thenReturn(new ArrayList<>(Arrays.asList(6,7,8,9,10)));
-        when(roleIdeal.getReadingLiteracy()).thenReturn(new ArrayList<>(Arrays.asList(6,7,8,9,10)));
-        when(roleIdeal.getCalculation()).thenReturn(new ArrayList<>(Arrays.asList(6,7,8,9,10)));
-        when(roleIdeal.getNumberComprehension()).thenReturn(new ArrayList<>(Arrays.asList(6,7,8,9,10)));
         when(personality.getStudyIndex()).thenReturn(7);
         when(personality.getVocabulary()).thenReturn(7);
         when(personality.getReadingLiteracy()).thenReturn(7);
